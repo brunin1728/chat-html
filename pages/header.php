a4c0c3aae0122cf5ea7e79d27bd535829131f7cb
Bruno Carvalho <brunocarvalho.info@gmail.com>
2025-05-14 15:16:36 -0300

    v1

---
+++ pages/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="pt-bt">
+
+    <head>
+        
+        <meta charset="utf-8" />
+        <title>Chat</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="" name="description" />
+        <meta name="keywords" content=""/>
+        <meta content="Themesbrand" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico" id="tabIcon">
+
+        <!-- glightbox css -->
+        <link rel="stylesheet" href="assets/libs/glightbox/css/glightbox.min.css">
+
+        <!-- swiper css -->
+        <link rel="stylesheet" href="assets/libs/swiper/swiper-bundle.min.css">
+
+        
+        <!-- Bootstrap Css -->
+        <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+
+
+    </head>
